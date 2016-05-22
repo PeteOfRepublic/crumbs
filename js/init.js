d5682f7307ac18dd542354012909abcf6a5b13bc
Flowdeeps <speedwolf@gmail.com>
2016-05-22 19:58:41 +0100

    Fixed my indenting

--- js/init.js
+++ js/init.js
@@ -1,102 +1,104 @@
 window.onload = function() {
-  var createCookie = document.getElementById('createCookie'),
-      createLocal  = document.getElementById('createLocal'),
-      createBoth   = document.getElementById('createBoth'),
-      readCookie   = document.getElementById('readCookie'),
-      readLocal    = document.getElementById('readLocal'),
-      readBoth     = document.getElementById('readBoth'),
-      clearCookie  = document.getElementById('clearCookie'),
-      clearLocal   = document.getElementById('clearLocal'),
-      clearBoth    = document.getElementById('clearBoth');
 
-      // stuff we're going to store for testing
-      var app = "Crumbs",
-          name = "Joe Bloggs",
-          age = 18,
-          faveFood = "Baked Beans",
-          stored = true;
+  // set dom elements
+  var createCookie = document.getElementById( 'createCookie' ),
+      createLocal  = document.getElementById( 'createLocal' ),
+      createBoth   = document.getElementById( 'createBoth' ),
+      readCookie   = document.getElementById( 'readCookie' ),
+      readLocal    = document.getElementById( 'readLocal' ),
+      readBoth     = document.getElementById( 'readBoth' ),
+      clearCookie  = document.getElementById( 'clearCookie' ),
+      clearLocal   = document.getElementById( 'clearLocal' ),
+      clearBoth    = document.getElementById( 'clearBoth' );
 
-        var expires = "; expires=Sun, 31 Dec 2017 12:00:00 UTC";
-        var destroy = "; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+  // stuff we're going to store for testing
+  var app = "Crumbs",
+      name = "Joe Bloggs",
+      age = 18,
+      faveFood = "Baked Beans",
+      stored = true;
 
-      // functions used multiple times
-      var storeCookie = function() {
-        console.log("store cookie");
-        document.cookie = "username=" + name + expires;
-        document.cookie = "age=" + age + expires;
-        document.cookie = "faveFood=" + faveFood + expires;
-      };
+  var expires = "; expires=Sun, 31 Dec 2017 12:00:00 UTC";
+  var destroy = "; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 
-      var storeLocal = function() {
-        console.log("store localStorage");
-        localStorage.setItem("name", name);
-        localStorage.setItem("age", age);
-        localStorage.setItem("faveFood", faveFood);
-      };
+  // functions used multiple times
+  var storeCookie = function() {
+    console.log("store cookie");
+    document.cookie = "username=" + name + expires;
+    document.cookie = "age=" + age + expires;
+    document.cookie = "faveFood=" + faveFood + expires;
+  };
 
-      var consoleCookie = function( event ) {
-        console.log(document.cookie);
-      };
+  var storeLocal = function() {
+    console.log( "store localStorage" );
+    localStorage.setItem( "name", name );
+    localStorage.setItem( "age", age );
+    localStorage.setItem( "faveFood", faveFood );
+  };
 
-      var consoleLocal = function( event ) {
-        console.log(localStorage);
-      };
+  var consoleCookie = function( event ) {
+    console.log(document.cookie);
+  };
 
-      var unstoreCookie = function() {
-        console.log( "unstore cookie" );
-        document.cookie = "username=" + destroy;
-        document.cookie = "age=" + destroy;
-        document.cookie = "faveFood=" + destroy;
-      };
+  var consoleLocal = function( event ) {
+    console.log(localStorage);
+  };
 
-      var unstoreLocal = function() {
-        console.log( "unstore localStorage" );
-        localStorage.clear();
-      };
+  var unstoreCookie = function() {
+    console.log( "unstore cookie" );
+    document.cookie = "username=" + destroy;
+    document.cookie = "age=" + destroy;
+    document.cookie = "faveFood=" + destroy;
+  };
 
-      // bind the actions to the relevent clicks
-      // cookie and localStorage creation events
-      createCookie.onclick = function( event ) {
-        event.preventDefault();
-        storeCookie();
-      }
-      createLocal.onclick = function( event ) {
-        event.preventDefault();
-        storeLocal();
-      }
-      createBoth.onclick = function( event ) {
-        event.preventDefault();
-        storeCookie();
-        storeLocal();
-      }
+  var unstoreLocal = function() {
+    console.log( "unstore localStorage" );
+    localStorage.clear();
+  };
 
-      // cookie and localStorage read events
-      readCookie.onclick = function( event ) {
-        event.preventDefault();
-        consoleCookie();
-      }
-      readLocal.onclick = function( event ) {
-        event.preventDefault();
-        consoleLocal();
-      }
-      readBoth.onclick = function( event ) {
-        event.preventDefault();
-        consoleCookie();
-        consoleLocal();
-      }
+  // bind the actions to the relevent clicks
+  // cookie and localStorage creation events
+  createCookie.onclick = function( event ) {
+    event.preventDefault();
+    storeCookie();
+  }
+  createLocal.onclick = function( event ) {
+    event.preventDefault();
+    storeLocal();
+  }
+  createBoth.onclick = function( event ) {
+    event.preventDefault();
+    storeCookie();
+    storeLocal();
+  }
 
-      // cookie and localStorage removal events
-      clearCookie.onclick = function( event ) {
-        event.preventDefault();
-        unstoreCookie();
-      }
-      clearLocal.onclick = function( event ) {
-        event.preventDefault();
-        unstoreLocal();
-      }
-      clearBoth.onclick = function( event ) {
-        event.preventDefault();
-        unstoreCookie();
-        unstoreLocal();
-      }
+  // cookie and localStorage read events
+  readCookie.onclick = function( event ) {
+    event.preventDefault();
+    consoleCookie();
+  }
+  readLocal.onclick = function( event ) {
+    event.preventDefault();
+    consoleLocal();
+  }
+  readBoth.onclick = function( event ) {
+    event.preventDefault();
+    consoleCookie();
+    consoleLocal();
+  }
+
+  // cookie and localStorage removal events
+  clearCookie.onclick = function( event ) {
+    event.preventDefault();
+    unstoreCookie();
+  }
+  clearLocal.onclick = function( event ) {
+    event.preventDefault();
+    unstoreLocal();
+  }
+  clearBoth.onclick = function( event ) {
+    event.preventDefault();
+    unstoreCookie();
+    unstoreLocal();
+  }
 }
